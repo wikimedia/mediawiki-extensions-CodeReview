wikimedia/mediawiki-extensions-CodeReview

commit 817a74bc3792f409c9b66eac9a894ef6777c24b5

Remove $this->skin calls for Linker
Remove unused parameters

Fixup some documentation

Change-Id: Iac42103b1d45cc64c87249cad7408b8b756683fc

backend/CodeRevision.php
@@ -646,8 +646,8 @@ class CodeRevision {
 	 * @param null $parent
 	 * @return CodeComment
 	 */
-	public function previewComment( $text, $review, $parent = null ) {
-		$data = $this->commentData( rtrim( $text ), $review, $parent );
+	public function previewComment( $text, $parent = null ) {
+		$data = $this->commentData( rtrim( $text ), $parent );
 		$data['cc_id'] = null;
 		return CodeComment::newFromData( $this, $data );
 	}
@@ -731,12 +731,11 @@ class CodeRevision {
 	}
 
 	/**
-	 * @param  $text
-	 * @param  $review
-	 * @param null $parent
+	 * @param $text string
+	 * @param $parent null|
 	 * @return array
 	 */
-	protected function commentData( $text, $review, $parent = null ) {
+	protected function commentData( $text, $parent = null ) {
 		global $wgUser;
 		$dbw = wfGetDB( DB_MASTER );
 		$ts = wfTimestamp( TS_MW );

backend/CodeComment.php
@@ -32,6 +32,7 @@ class CodeComment {
 	 * @param int $cc_id Comment ID in the database
 	 * @param CodeRevision $rev A revision object to which the comment is
 	 * attached
+	 * @return CodeComment
 	 */
 	static function newFromID( $cc_id, CodeRevision $rev ) {
 		$dbr = wfGetDB( DB_SLAVE );

backend/CodeCommentLinker.php
@@ -1,12 +1,6 @@
 <?php
 
 abstract class CodeCommentLinker {
-
-	/**
-	 * @var Skin
-	 */
-	protected $skin;
-
 	/**
 	 * @var CodeRepository
 	 */
@@ -16,8 +10,6 @@ abstract class CodeCommentLinker {
 	 * @param $repo CodeRepository
 	 */
 	function __construct( $repo ) {
-		global $wgUser;
-		$this->skin = $wgUser->getSkin();
 		$this->mRepo = $repo;
 	}
 
@@ -64,6 +56,7 @@ abstract class CodeCommentLinker {
 
 	/**
 	 * @param $matches array
+	 * @return string
 	 */
 	function messageRevLink( $matches ) {
 		$text = $matches[0];
@@ -82,6 +75,12 @@ abstract class CodeCommentLinker {
 	 */
 	abstract function makeExternalLink( $url, $text );
 
+	/**
+	 * @abstract
+	 * @param $title Title
+	 * @param $text string
+	 * @return string
+	 */
 	abstract function makeInternalLink( $title, $text );
 }
 
@@ -93,7 +92,7 @@ class CodeCommentLinkerHtml extends CodeCommentLinker {
 	 * @return string
 	 */
 	function makeExternalLink( $url, $text ) {
-		return $this->skin->makeExternalLink( $url, $text );
+		return Linker::makeExternalLink( $url, $text );
 	}
 
 	/**
@@ -102,7 +101,7 @@ class CodeCommentLinkerHtml extends CodeCommentLinker {
 	 * @return  string
 	 */
 	function makeInternalLink( $title, $text ) {
-		return $this->skin->link( $title, $text );
+		return Linker::link( $title, $text );
 	}
 }
 

backend/CodeRepository.php
@@ -159,7 +159,7 @@ class CodeRepository {
 	 * Return a bug URL or false
 	 *
 	 * @param $bugId int|string
-	 * @return string|false.
+	 * @return string|bool
 	 */
 	public function getBugPath( $bugId ) {
 		if ( $this->bugzilla ) {
@@ -251,6 +251,7 @@ class CodeRepository {
 	/**
 	 * Load a particular revision out of the DB
 	 * @param $id int|string
+	 * @throws MWException
 	 * @return CodeRevision
 	 */
 	public function getRevision( $id ) {
@@ -559,7 +560,7 @@ class CodeRepository {
 	 *
 	 * @param $name string
 	 *
-	 * @return string|false
+	 * @return string|bool
 	 */
 	public function wikiUserAuthor( $name ) {
 		if ( isset( self::$authorLinks[$name] ) )

backend/DiffHighlighter.php
@@ -54,13 +54,13 @@ class CodeDiffHighlighter {
 		switch( substr( $line, 0, 1 ) ) {
 		case '-':
 			if( substr( $line, 0, 3 ) === '---' ) {
-				return;
+				return '';
 			}
 			$r = $this->handleLineDeletion( $line );
 			break;
 		case '+':
 			if( substr( $line, 0, 3 ) === '+++' ) {
-				return;
+				return '';
 			}
 			$r = $this->handleLineAddition( $line );
 			break;
@@ -73,7 +73,7 @@ class CodeDiffHighlighter {
 
 		# Patch lines that will be skipped:
 		case '=':
-			return;
+			return '';
 
 		# Remaining case should be the file name
 		default:
@@ -84,6 +84,11 @@ class CodeDiffHighlighter {
 		return $r;
 	}
 
+	/**
+	 * @param $content string
+	 * @param $class string
+	 * @return string
+	 */
 	function formatLine( $content, $class = null ) {
 
 		if ( $class === null ) {
@@ -227,7 +232,8 @@ class CodeDiffHighlighter {
 	 *
 	 * NOTE: visibility is 'public' since the function covered by tests.
 	 *
-	 * @param $chunk string a one line chunk as described above
+	 * @param $chunkHeader string a one line chunk as described above
+	 * @throws MWException
 	 * @return array with the four values above as an array
 	 */
 	static function parseChunkDelimiter( $chunkHeader ) {

backend/Subversion.php
@@ -23,7 +23,7 @@ abstract class SubversionAdaptor {
 	}
 
 	/**
-	 * @param $repo String Path to SVN Repo
+	 * @param $repoPath String Path to SVN Repo
 	 */
 	function __construct( $repoPath ) {
 		$this->mRepoPath = $repoPath;

ui/CodeRevisionAuthorLink.php
@@ -74,7 +74,7 @@ class CodeRevisionAuthorLink extends CodeRevisionAuthorView {
 				return;
 			}
 			$this->mRepo->linkUser( $this->mAuthor, $user );
-			$userlink = $this->skin->userLink( $user->getId(), $user->getName() );
+			$userlink = Linker::userLink( $user->getId(), $user->getName() );
 			$wgOut->addHTML(
 				'<div class="successbox">' .
 				wfMsgHtml( 'code-author-success', $this->authorLink( $this->mAuthor ), $userlink ) .

ui/CodeRevisionAuthorView.php
@@ -18,8 +18,8 @@ class CodeRevisionAuthorView extends CodeRevisionListView {
 		}
 
 		return wfMsgHtml( 'code-author-haslink',
-			$this->skin->userLink( $this->mUser->getId(), $this->mUser->getName() ) .
-			$this->skin->userToolLinks(
+			Linker::userLink( $this->mUser->getId(), $this->mUser->getName() ) .
+			Linker::userToolLinks(
 				$this->mUser->getId(),
 				$this->mUser->getName(),
 				false, /* default for redContribsWhenNoEdits */
@@ -35,11 +35,11 @@ class CodeRevisionAuthorView extends CodeRevisionListView {
 		if ( $wgUser->isAllowed( 'codereview-link-user' ) ) {
 			$repo = $this->mRepo->getName();
 			$page = SpecialPage::getTitleFor( 'Code', "$repo/author/$this->mAuthor/link" );
-			$linkInfo .= ' (' . $this->skin->link( $page,
+			$linkInfo .= ' (' . Linker::link( $page,
 				wfMsg( 'code-author-' . ( $this->mUser ? 'un':'' ) . 'link' ) ) . ')' ;
 		}
 
-		$repoLink = $this->skin->link( SpecialPage::getTitleFor( 'Code', $this->mRepo->getName() ),
+		$repoLink = Linker::link( SpecialPage::getTitleFor( 'Code', $this->mRepo->getName() ),
 			htmlspecialchars( $this->mRepo->getName() ) );
 		$fields = array(
 			'code-rev-repo' => $repoLink,

commit 78141b9375526a844febd645805df9bb17fd43f1

Correctly nest comments.
Fixes bug #46636.

This was caused by 817a74bc3792f409c9b66eac9a894ef6777c24b5, where
commentData()'s middle parameter ($review) was removed, but the call to
commentData() in saveComment() was not updated accordingly.

Based on my very limited tests, $review seems to be 0 always, whereas
$parent is the parent comment's ID when we're replying to a comment.
Because the parameter order was changed when one parameter was removed,
$review unintentionally became the new $parent, and as a result, all
comments were treated as new top-level comments, even if they were replies
to other comments.

Also bumped version number.

Change-Id: I8d6d576fa9f3c15bef2e19310efc0b2265f8a618

CodeReview.php
@@ -34,7 +34,7 @@ http://pecl.php.net/package/svn
 $wgExtensionCredits['specialpage'][] = array(
 	'path' => __FILE__,
 	'name' => 'CodeReview',
-	'version' => '1.13',
+	'version' => '1.14',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:CodeReview',
 	'author' => array( 'Brion Vibber', 'Aaron Schulz', 'Alexandre Emsenhuber', 'Chad Horohoe', 'Sam Reed', 'Roan Kattouw' ),
 	'descriptionmsg' => 'codereview-desc',

api/ApiRevisionUpdate.php
@@ -67,6 +67,7 @@ class ApiRevisionUpdate extends ApiBase {
 			$params['addreferences'],
 			$params['removereferences'],
 			$params['comment'],
+			null,
 			$params['addreferenced'],
 			$params['removereferenced']
 		);

backend/CodeRevision.php
@@ -651,18 +651,17 @@ class CodeRevision {
 	}
 
 	/**
-	 * @param  $text
-	 * @param  $review
-	 * @param null $parent
+	 * @param $text string
+	 * @param $parent null
 	 * @return int
 	 */
-	public function saveComment( $text, $review, $parent = null ) {
+	public function saveComment( $text, $parent = null ) {
 		$text = rtrim( $text );
 		if ( !strlen( $text ) ) {
 			return 0;
 		}
 		$dbw = wfGetDB( DB_MASTER );
-		$data = $this->commentData( $text, $review, $parent );
+		$data = $this->commentData( $text, $parent );
 
 		$dbw->begin();
 		$data['cc_id'] = $dbw->nextSequenceValue( 'code_comment_cc_id' );

ui/CodeRevisionCommitter.php
@@ -14,11 +14,18 @@ class CodeRevisionCommitter extends CodeRevisionView {
 			return;
 		}
 
-		$commentId = $this->revisionUpdate( $this->mStatus, $this->mAddTags, $this->mRemoveTags,
-			$this->mSignoffFlags, $this->mStrikeSignoffs,
-			$this->mAddReferences, $this->mRemoveReferences,
-			$this->text, $wgRequest->getIntOrNull( 'wpParent' ), $wgRequest->getInt( 'wpReview' ),
-			$this->mAddReferenced, $this->mRemoveReferenced
+		$commentId = $this->revisionUpdate(
+			$this->mStatus,
+			$this->mAddTags,
+			$this->mRemoveTags,
+			$this->mSignoffFlags,
+			$this->mStrikeSignoffs,
+			$this->mAddReferences,
+			$this->mRemoveReferences,
+			$this->text,
+			$wgRequest->getIntOrNull( 'wpParent' ),
+			$this->mAddReferenced,
+			$this->mRemoveReferenced
 		);
 
 		$redirTarget = null;
@@ -65,8 +72,7 @@ class CodeRevisionCommitter extends CodeRevisionView {
 	 */
 	public function revisionUpdate( $status, $addTags, $removeTags, $addSignoffs, $strikeSignoffs,
 						$addReferences, $removeReferences, $commentText,
-						$parent = null, $review = 0,
-						$addReferenced, $removeReferenced
+						$parent = null, $addReferenced, $removeReferenced
 					) {
 		if ( !$this->mRev ) {
 			return false;
@@ -123,7 +129,7 @@ class CodeRevisionCommitter extends CodeRevisionView {
 		$commentId = 0;
 		if ( strlen( $commentText ) && $this->validPost( 'codereview-post-comment' ) ) {
 			// $isPreview = $wgRequest->getCheck( 'wpPreview' );
-			$commentId = $this->mRev->saveComment( $commentText, $review, $parent );
+			$commentId = $this->mRev->saveComment( $commentText, $parent );
 
 			$commentAdded = ($commentId !== 0);
 		}