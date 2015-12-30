0ce7f372f656083d271da54257640ba3aa4481bb
Aaron Schulz <aschulz@wikimedia.org>
2015-12-30 15:27:31 -0800

    Use __METHOD__ for DB begin/commit calls
    Change-Id: I8330e9b41fc0d94398ed0a6b415592486515656d

--- maintenance/deleteBadTags.php
+++ maintenance/deleteBadTags.php
@@ -11,13 +11,13 @@ echo "Usage: php deleteBadTags.php [commit]\n";
 echo "Deleting empty tags...\n";
 
 $dbw = wfGetDB( DB_MASTER );
-$dbw->begin();
+$dbw->begin( __METHOD__ );
 $dbw->delete( 'code_tags', array('ct_tag' => ''), __METHOD__ );
 $count = $dbw->affectedRows();
 if( isset($args[0]) && $args[0] == 'commit' ) {
-	$dbw->commit();
+	$dbw->commit( __METHOD__ );
 	echo "$count bad tags deleted. Done!\n";
 } else {
-	$dbw->rollback();
+	$dbw->rollback( __METHOD__ );
 	echo "$count bad tags. Not commited!\n";
 }
