46c799ae09a0cc7f3827898088990ec631a1c65a
Reedy <reedy@wikimedia.org>
2016-07-29 20:26:36 +0100

    Update deleteBadTags to use Maintenance rather than commandLine.inc
    Change-Id: Ibfffef89cce885c712c326507c0f0e9c8ca0f3af

--- maintenance/deleteBadTags.php
+++ maintenance/deleteBadTags.php
@@ -4,20 +4,31 @@ $IP = getenv( 'MW_INSTALL_PATH' );
 if ( $IP === false ) {
 	$IP = __DIR__ . '/../../..';
 }
-require_once( "$IP/maintenance/commandLine.inc" );
+require_once( "$IP/maintenance/Maintenance.php" );
 
-echo "Usage: php deleteBadTags.php [commit]\n";
+class DeleteBadTags extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = "Delete empty Code Review tags";
+		$this->addOption( 'dry-run', 'Don\'t actually delete bad tags, just compile statistics.' );
+	}
 
-echo "Deleting empty tags...\n";
+	public function execute() {
+		$dbw = wfGetDB( DB_MASTER );
+		$dbw->begin( __METHOD__ );
+		$dbw->delete( 'code_tags', array('ct_tag' => ''), __METHOD__ );
+		$count = $dbw->affectedRows();
 
-$dbw = wfGetDB( DB_MASTER );
-$dbw->begin( __METHOD__ );
-$dbw->delete( 'code_tags', array('ct_tag' => ''), __METHOD__ );
-$count = $dbw->affectedRows();
-if( isset($args[0]) && $args[0] == 'commit' ) {
-	$dbw->commit( __METHOD__ );
-	echo "$count bad tags deleted. Done!\n";
-} else {
-	$dbw->rollback( __METHOD__ );
-	echo "$count bad tags. Not commited!\n";
+		$this->output( "Deleting empty tags...\n" );
+		if( !$this->getOption( 'dry-run' ) ) {
+			$dbw->commit( __METHOD__ );
+			$this->output( "$count bad tags deleted. Done!\n" );
+		} else {
+			$dbw->rollback( __METHOD__ );
+			$this->output( "$count bad tags. Not committed!\n" );
+		}
+	}
 }
+
+$maintClass = "DeleteBadTags";
+require_once( DO_MAINTENANCE );
