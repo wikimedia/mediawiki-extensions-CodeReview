417dda6cd7b8676b811fa14fb69643479dfccbef
zoranzoki21 <zorandori4444@gmail.com>
2020-03-14 21:01:02 +0100

    Move SvnRevStatusTablePager class into the own file
    Change-Id: I561fbeb80ae5e056c237dcf349f7ffca634d8e7c

--- includes/ui/CodeRevisionStatusView.php
+++ includes/ui/CodeRevisionStatusView.php
@@ -10,21 +10,3 @@ class CodeRevisionStatusView extends CodeRevisionListView {
 		return new SvnRevStatusTablePager( $this, $this->mStatus );
 	}
 }
-
-class SvnRevStatusTablePager extends SvnRevTablePager {
-	public function __construct( $view, $status ) {
-		parent::__construct( $view );
-		$this->mStatus = $status;
-	}
-
-	public function getQueryInfo() {
-		$info = parent::getQueryInfo();
-		$info['conds']['cr_status'] = $this->mStatus; // FIXME: normalize input?
-		return $info;
-	}
-
-	public function getTitle() {
-		$repo = $this->mRepo->getName();
-		return SpecialPage::getTitleFor( 'Code', "$repo/status/$this->mStatus" );
-	}
-}
