e19974d36699aca042e93e5e4cd90ad9f8948f03
zoranzoki21 <zorandori4444@gmail.com>
2020-03-14 16:59:33 +0100

    Move CodeCommentLinkerWiki class into the own file
    Change-Id: I1c31efd05f0d76b39532a0da3e2dd65f50e89142

--- includes/backend/CodeCommentLinker.php
+++ includes/backend/CodeCommentLinker.php
@@ -85,23 +85,3 @@ abstract class CodeCommentLinker {
 	 */
 	abstract public function makeInternalLink( $title, $text );
 }
-
-class CodeCommentLinkerWiki extends CodeCommentLinker {
-	/**
-	 * @param string $url
-	 * @param string $text
-	 * @return string
-	 */
-	public function makeExternalLink( $url, $text ) {
-		return "[$url $text]";
-	}
-
-	/**
-	 * @param Title $title
-	 * @param string $text
-	 * @return string
-	 */
-	public function makeInternalLink( $title, $text ) {
-		return '[[' . $title->getPrefixedText() . "|$text]]";
-	}
-}
