2e6c6bcab9d5b2631d83c474f29d639852f9a33d
zoranzoki21 <zorandori4444@gmail.com>
2020-03-14 16:56:45 +0100

    Move CodeCommentLinkerHtml class into the own file
    Change-Id: I3a7392453b60aef0d12f15cc7a846601ebda19e6

--- includes/backend/CodeCommentLinker.php
+++ includes/backend/CodeCommentLinker.php
@@ -86,28 +86,6 @@ abstract class CodeCommentLinker {
 	abstract public function makeInternalLink( $title, $text );
 }
 
-class CodeCommentLinkerHtml extends CodeCommentLinker {
-
-	/**
-	 * @param string $url
-	 * @param string $text
-	 * @return string
-	 */
-	public function makeExternalLink( $url, $text ) {
-		return Linker::makeExternalLink( $url, $text );
-	}
-
-	/**
-	 * @param Title $title
-	 * @param string $text
-	 * @return string
-	 */
-	public function makeInternalLink( $title, $text ) {
-		$linkRenderer = \MediaWiki\MediaWikiServices::getInstance()->getLinkRenderer();
-		return $linkRenderer->makeLink( $title, $text );
-	}
-}
-
 class CodeCommentLinkerWiki extends CodeCommentLinker {
 	/**
 	 * @param string $url
